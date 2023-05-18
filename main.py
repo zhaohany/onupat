bef33b42155c7cc4fcbcd3520304e495c1807a6f
zhaohany <zhaohany@outlook.com>
2023-05-18 11:49:10 -0400

    feat: performance enhancement

--- main.py
+++ main.py
@@ -1,39 +1,26 @@
 import streamlit as st
 from transformers import pipeline, set_seed
+import json
+import random
 
 generator = pipeline('text-generation', model='gpt2')
 
-def remove_punctuation(test_str):
-  result = ''.join(filter(lambda x: x.isalpha() or x.isdigit() or x.isspace(), test_str))
-  return result
+with open('prompt.json') as json_file:
+    promt_data = json.load(json_file)
+
+def get_promt(key):
+    return random.choice(promt_data[key])["description"]
+
 
 def select_templates():
-    return "Once upon a time, in <location> <loc_desc>, there lived a <char_prop> <char_job> named <char_name>."
-
-def generate_location():
-    generated_text = generator("A location called", max_length=7, num_return_sequences=1)
-    return remove_punctuation(generated_text[0]['generated_text'].split("A location called ")[1])
-
-def generate_loc_desc():
-    generated_text = generator("One word to describe a location is", max_length=11, num_return_sequences=1)
-    return remove_punctuation(generated_text[0]['generated_text'].split("One word to describe a location is ")[1])
-
-def generate_character_properties():
-    generated_text = generator("One word to describe a person is", max_length=11, num_return_sequences=1)
-    return remove_punctuation(generated_text[0]['generated_text'].split("One word to describe a person is ")[1])
-
-def generate_character_job():
-    generated_text = generator("A job called", max_length=7, num_return_sequences=1)
-    return remove_punctuation(generated_text[0]['generated_text'].split("A job called ")[1])
+    return "Once upon a time, in <location>, there lived a <char_prop> <char_job> named <char_name>."
 
 def generate_story(story_promt):
-    generated_text = generator(story_promt, max_length=150, num_return_sequences=1)
-    return generated_text[0]['generated_text'].rsplit('.', 1)[0]
-
-def generate_end():
-    generated_text = generator("At the end of story, ", max_length=30, num_return_sequences=1)
-    return generated_text[0]['generated_text'].rsplit('.', 1)[0]
-
+    try:
+        generated_text = generator(story_promt, max_length=150, num_return_sequences=1)
+        return generated_text[0]['generated_text'].rsplit('.', 1)[0]
+    except:
+        return story_promt
 
 
 char_name = st.text_input('Character Name', 'Cinderella')
@@ -43,13 +30,11 @@ generate = st.button('Generate')
 story_text = 'Once upon a time ......'
 if generate:
     story_prompt = select_templates()
-    story_prompt = story_prompt.replace("<location>",generate_location())
-    story_prompt = story_prompt.replace("<loc_desc>",generate_loc_desc())
-    story_prompt = story_prompt.replace("<char_prop>",generate_character_properties())
-    story_prompt = story_prompt.replace("<char_job>",generate_character_job())
+    story_prompt = story_prompt.replace("<location>",get_promt("locations"))
+    story_prompt = story_prompt.replace("<char_prop>",get_promt("properties"))
+    story_prompt = story_prompt.replace("<char_job>",get_promt("jobs"))
     story_prompt = story_prompt.replace("<char_name>",char_name)
-    story_text = generate_story(story_prompt)+". "+generate_end()
-
+    story_text = generate_story(story_prompt)+"."
 
 
 story = st.text_area('Your Story', story_text)
