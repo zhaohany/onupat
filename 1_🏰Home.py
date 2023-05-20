4a1888ffb18da44be8025dc23d94b78c75134154
zhaohany <zhaohany@outlook.com>
2023-05-20 00:48:28 -0400

    fix: ga

--- 1_🏰Home.py
+++ 1_🏰Home.py
@@ -4,15 +4,10 @@ from PIL import Image
 import os
 import json
 from streamlit_lottie import st_lottie
-import pathlib
-from bs4 import BeautifulSoup
-import shutil
+import re
 
 def inject_ga():
-    GA_ID = "google_analytics"
-
-
-    GA_JS = """
+    code = """
       <!-- Google tag (gtag.js) -->
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-RC807JENT2"></script>
       <script>
@@ -24,19 +19,13 @@ def inject_ga():
       </script>
       <meta name="google-site-verification" content="JSXvvpNZ6wcizZbxH7tzCfENZ46N1l2QSo56OGjgMYE" />
     """
-
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copy(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
+    a=os.path.dirname(st.__file__)+'/static/index.html'
+    with open(a, 'r') as f:
+       data=f.read()
+       if len(re.findall('G-RC807JENT2', data))==0:
+        with open(a, 'w') as ff:
+            newdata=re.sub('<head>','<head>'+code,data)
+            ff.write(newdata)
 
 inject_ga()
 
